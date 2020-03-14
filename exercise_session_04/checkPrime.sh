0fbc9f75d5a58e921514b13e810a707f62ecd9ad
Crs03 Course <course93@daint102.login.cscs.ch>
2020-03-14 16:42:17 +0100

    most inefficient prime check ever is working

--- exercise_session_04/checkPrime.sh
+++ exercise_session_04/checkPrime.sh
@@ -1,11 +1,27 @@
-if !  [[ $1 =~ ^[0-9]+$ ]]
-	then 
-		echo integers only!
-else
-  END=$1
-	for index in {2..$END}
-	do
-		echo $index
-	done
+is_prime () {
+  for i in $(seq 2 $number) 
+  do
+    echo $i
+    if test `expr $number % $i` -eq 0
+    then
+      echo not prime
+      return 0
+    fi
+    echo is prime
+    return 1
+  done    
+}
 
+if [[ $# == 0 ]]
+  then
+    read -p "Enter number:" number
+else
+  number=$1
+fi
+
+if ! [[ $number =~ ^[0-9]+$ ]]
+  then
+    echo integers only!
+else
+  is_prime
 fi
