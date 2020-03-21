e5c491f601d05db336b3c92794705c49f928a4cf
Crs03 Course <course93@daint105.login.cscs.ch>
2020-03-21 16:44:56 +0100

    progress on exercise 5

--- exercise_session_05/rings_and_deadlocks/ring.f90
+++ exercise_session_05/rings_and_deadlocks/ring.f90
@@ -11,7 +11,7 @@ program ring
   implicit none
   
   ! Variables declaration
-  integer :: my_rank, n_proc, ierror
+  integer :: my_rank, n_proc, ierror, i, total, neigh, left
 
   ! Begin MPI
   call MPI_INIT(ierror)
@@ -19,10 +19,21 @@ program ring
   ! Get my_rank and n_proc
   call MPI_COMM_RANK(MPI_COMM_WORLD, my_rank, ierror)
   call MPI_COMM_SIZE(MPI_COMM_WORLD, n_proc, ierror)
-  
 
-  write(*,*) 'I am processor ',my_rank,' out of ',n_proc
+  do i = 0, n_proc, +1
+     
+    neigh = modulo(my_rank + 1,  n_proc)
+    call MPI_SEND(my_rank, 1, MPI_INT, neigh, 1, MPI_COMM_WORLD, ierror)
 
+    neigh = modulo(my_rank - 1, n_proc)
+    call MPI_RECV(left, 1, MPI_INT, neigh, 1, MPI_COMM_WORLD, ierror, MPI_STATUS_IGNORE)
+
+    total = total + left
+  end do
+ 
+  write(*,*) 'I am processor', my_rank,' out of ', n_proc,'and the sum is', total
+
+ 
   call MPI_FINALIZE (ierror)
 
 end program ring
