057961c938125a6fcd5640fcd7e7e0893e455695
Crs03 Course <course93@daint105.login.cscs.ch>
2020-03-22 21:02:55 +0100

    progress on euler

--- exercise_session_05/euler.f90
+++ exercise_session_05/euler.f90
@@ -12,8 +12,9 @@ program euler
   
   ! Variables declaration
   integer :: my_rank, n_proc, ierror
+  INTEGER (KIND=8) N, part, rest, i
+  COMPLEX (KIND=16) subsum, total, current
 
-  INTEGER (KIND = 8) N, part, rest, i
   ! Begin MPI
   call MPI_INIT(ierror)
   
@@ -25,16 +26,35 @@ program euler
  
   IF (my_rank == 0) THEN
     part = N / n_proc
-    rest = N - n_proc*part
 
-    do i = 1, n_proc, +1
-      call MPI_SEND(part, 1, MPI_LONG_LONG_INT, i, MPI_COMM_WORLD, ierror)
+    do i = 1, n_proc-1, +1
+      call MPI_SEND(part, 1, MPI_LONG_LONG_INT, i, 1, MPI_COMM_WORLD, ierror)
     end do
+
+    rest = N - n_proc*part
   ELSE
-    call MPI_RECV(part, 1, MPI_LONG_LONG_INT, 0, MPI_COMM_WORLD, ierror)
+    call MPI_RECV(part, 1, MPI_LONG_LONG_INT, 0, 1, MPI_COMM_WORLD, ierror, MPI_STATUS_IGNORE)
   END IF
 
-  write(*,*) part
+  current = 1
+  do i = part*my_rank, part*(my_rank + 1), +1
+    current = current * 1. / i
+    subsum = subsum + subsum * current
+  end do
+  
+  IF (my_rank == 0) THEN
+    do i = 1, n_proc-1, +1
+      call MPI_RECV(current, 1, MPI_LONG_DOUBLE, i, 1, MPI_COMM_WORLD, ierror, MPI_STATUS_IGNORE)
+      call MPI_RECV(subsum, 1, MPI_LONG_DOUBLE, i, 2, MPI_COMM_WORLD, ierror, MPI_STATUS_IGNORE)
+      
+       
+    end do
+  ELSE
+    call MPI_SEND(current, 1, MPI_LONG_DOUBLE, 0, 1, MPI_COMM_WORLD, ierror)
+    call MPI_SEND(subsum, 1, MPI_LONG_DOUBLE, 0, 2, MPI_COMM_WORLD, ierror)
+  END IF
+
+  write(*,*) subsum
  
   call MPI_FINALIZE (ierror)
 
