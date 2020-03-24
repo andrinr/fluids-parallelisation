2c57596bd07752d472482857a498a96f55fc918d
Crs03 Course <course93@daint107.login.cscs.ch>
2020-03-24 09:40:20 +0100

    finished euler, not wokring because numbers exceed max size

--- exercise_session_05/euler.f90
+++ exercise_session_05/euler.f90
@@ -11,9 +11,15 @@ program euler
   implicit none
   
   ! Variables declaration
-  integer :: my_rank, n_proc, ierror
-  INTEGER (KIND=8) N, part, rest, i
-  COMPLEX (KIND=16) subsum, total, current
+  integer my_rank, n_proc, ierror
+  integer N, part, rest, i, divisor, divisorTotal
+  double precision subsum, total
+
+  ! Variable initialisation
+  total = 0.
+  subsum = 1.
+  divisor = 1
+  divisorTotal = 1
 
   ! Begin MPI
   call MPI_INIT(ierror)
@@ -35,27 +41,28 @@ program euler
   ELSE
     call MPI_RECV(part, 1, MPI_LONG_LONG_INT, 0, 1, MPI_COMM_WORLD, ierror, MPI_STATUS_IGNORE)
   END IF
-
-  current = 1
+  write(*,*) 'subsum: ', divisor, subsum, part
   do i = part*my_rank, part*(my_rank + 1), +1
-    current = current * 1. / i
-    subsum = subsum + subsum * current
+    divisor = divisor * 1./i
+    subsum = subsum + subsum * divisor
   end do
+
+  write(*,*) 'subsum: ', subsum, 'current: ', divisor
   
   IF (my_rank == 0) THEN
     do i = 1, n_proc-1, +1
-      call MPI_RECV(current, 1, MPI_LONG_DOUBLE, i, 1, MPI_COMM_WORLD, ierror, MPI_STATUS_IGNORE)
-      call MPI_RECV(subsum, 1, MPI_LONG_DOUBLE, i, 2, MPI_COMM_WORLD, ierror, MPI_STATUS_IGNORE)
-      
-       
+      call MPI_RECV(divisor, 1, MPI_LONG_LONG_INT , i, 1, MPI_COMM_WORLD, ierror, MPI_STATUS_IGNORE)
+      divisorTotal = divisorTotal * divisor
+      call MPI_RECV(subsum, 1, MPI_DOUBLE , i, 2, MPI_COMM_WORLD, ierror, MPI_STATUS_IGNORE)
+      total = total + subsum * 1. / divisorTotal
     end do
+
+    write(*,*) 'Euler approximation: ', total
   ELSE
-    call MPI_SEND(current, 1, MPI_LONG_DOUBLE, 0, 1, MPI_COMM_WORLD, ierror)
-    call MPI_SEND(subsum, 1, MPI_LONG_DOUBLE, 0, 2, MPI_COMM_WORLD, ierror)
+    call MPI_SEND(divisor, 1, MPI_LONG_LONG_INT , 0, 1, MPI_COMM_WORLD, ierror)
+    call MPI_SEND(subsum, 1, MPI_DOUBLE , 0, 2, MPI_COMM_WORLD, ierror)
   END IF
 
-  write(*,*) subsum
- 
   call MPI_FINALIZE (ierror)
 
 end program euler
