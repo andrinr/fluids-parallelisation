2a61013b72d29e1cf583d6bf711aae4419dd7c63
Crs03 Course <course93@daint103.login.cscs.ch>
2020-04-01 16:27:51 +0200

    finished ex 2.1 with cartesion mpi

--- exercise_session_06/poisson_solver/Src/mpi_module.f90
+++ exercise_session_06/poisson_solver/Src/mpi_module.f90
@@ -4,11 +4,15 @@ module poisson_mpi
 
   implicit none
   integer, parameter :: tag = 123
-  integer :: myrank, nproc, ierror
+  integer :: myrank, nproc, ierror, COMM_CART
   integer :: ndims = 2
-  integer :: myleft, myright
-  integer :: myimin, myimax, myn, rest
-  logical :: boundary_left, boundary_right
+  integer :: myleft, myright,  mybot, mytop
+  integer :: myn, rest
+  integer, dimension(2) :: dimensions, coords, mymin, mymax = (/0,0/)
+  logical, dimension(2) :: periods = (/.True. , .True./)
+  logical :: boundary_left, boundary_right, boundary_bot, boundary_top
+  logical :: reorder = .true.
+
   
 contains
 
@@ -17,36 +21,61 @@ subroutine init_mpi
   use poisson_parameters
 
   call MPI_COMM_SIZE(MPI_COMM_WORLD, nproc, ierror)
-  call MPI_DIMS_CREATE(size, ndims, dims)
-  call MPI_CART_CREATE(MPI_COMM_WORLD, ndims, dims, periods, reorder, COMM_CART, ierror)
+  call MPI_DIMS_CREATE(nproc, ndims, dimensions, ierror)
+  call MPI_CART_CREATE(MPI_COMM_WORLD, ndims, dimensions, periods, reorder, COMM_CART, ierror)
   call MPI_COMM_RANK(COMM_CART, myrank, ierror)
+  call MPI_CART_COORDS(COMM_CART, myrank, dimensions, coords, ierror)
 
   if (myrank==0) then
     print *
     print *,' MPI Execution with ',nproc,' processes'
+    print *,' Cartesian grid of size ', dimensions
     print *,' Starting time integration, nx = ',nx,' ny = ',ny
     print *
   endif
+
+  print *,'I am proc ',myrank,' and my coordinates are ', coords
 
   !
   ! Setup ranks of left and right neighbors.
   ! If there is no neighbor in some <direction>,  boundary_<direction> must be .false.
   ! Otherwise, boundary_<direction> must be .true.
-  ! 
-  myleft = myrank - 1
-  myright = myrank + 1
+  !
+  call MPI_CART_SHIFT(COMM_CART, 0, 1, myleft, myright, ierror)
+  call MPI_CART_SHIFT(COMM_CART, 1, 1, mybot, mytop, ierror)
+
   boundary_right = .true.
   boundary_left = .true.
-  
-  if (myrank == 0) then
+
+  ! Default slab size
+  mymin(1) = coords(1) * nx / dimensions(1)
+  mymin(2) = coords(2) * ny / dimensions(2)
+
+  mymax(1) = ( 1 + coords(1) ) * nx / dimensions(1) - 1
+  mymax(2) = ( 1 + coords(2) ) * ny / dimensions(2) - 1
+
+  if (coords(1) == 0) then
     boundary_left = .false.
     ! Safe definition of neighbor rank : any communication will be ignored.
     myleft = MPI_PROC_NULL
-  else if (myrank == nproc-1) then
+  else if (coords(1) == dimensions(1) - 1) then
     boundary_right = .false.
     ! Safe definition of neighbor rank : any communication will be ignored.
-    myright = MPI_PROC_NULL
+    mytop = MPI_PROC_NULL
+    mymax(1) = nx
   end if
+
+  if (coords(2) == 0) then
+    boundary_bot = .false.
+    ! Safe definition of neighbor rank : any communication will be ignored.
+    mybot = MPI_PROC_NULL
+  else  if (coords(2) == dimensions(2) - 1) then
+    boundary_right = .false.
+    ! Safe definition of neighbor rank : any communication will be ignored.
+    mytop = MPI_PROC_NULL
+    mymax(2) = ny
+  end if
+
 
   ! Setup local domain range : each process take care of a slab [myimin,myimax]x[1,ny]
   ! For example : nx=9, ny=6 with 3 processes
@@ -66,9 +95,9 @@ subroutine init_mpi
   ! I   : if nx/nproc is not integer, the last process can handle the remaining columns of the domain.
   ! II  : other option, the remaining columns of the domain can be shared between the processes.
   ! III : other (harder) approach : the domain could be decomposed in both x and y direction.
-  myimin=1
-  myimax=1
-  print*, 'I am proc ',myrank,' and my x-domain goes from ',myimin,' to ',myimax
+
+  print*, 'I am proc ',myrank,' and my x-domain goes from ',mymin(1),' to ',mymax(1)
+  print*, 'I am proc ',myrank,' and my y-domain goes from ',mymin(2),' to ',mymax(2)
   
 end subroutine init_mpi
 
